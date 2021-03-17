e0ffc2be70d97b7f992ad856edbcf77766a474f2
truongleipod2012 <truongleipod2012@gmail.com>
2021-03-17 18:27:56 +0700

    update A1

--- BT08/A1.cpp
+++ BT08/A1.cpp
@@ -20,3 +20,4 @@ int main()
 }
 
 // Khi chạy chương trình, địa chị cout của 2 biến sẽ giống nhau, ta có thể lí giải vì trong hàm main, ta đã gọi hàm f và g lần lượt, khi chương trình gán cho f 1 địa chỉ và chạy đã xong chuyển sang hàm g, khả năng cao chương trình sẽ vẫn dùng lại địa chỉ từ hàm f gán với g.
+//truong
